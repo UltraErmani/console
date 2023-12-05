bdb9ae74305f5cd3c40759573891e1f2ef865f35
UltraErmani <139281495+UltraErmani@users.noreply.github.com>
2023-12-05 17:57:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let nome = "Julio ";
+let sobrenome = "Ermani Rodrigues "
+
+let nomecompleto = nome.concat(sobrenome);
+console.log (nomecompleto);
+
+let comprimento = nomecompleto.length;
+
+console.log (comprimento);
+
+let chato = nomecompleto.replace("Julio","Andre");
+console.log (chato);
+
+let a = Math.random()*100;
+console.log(a);
+let usardecimais = a.toFixed(2);
+console.log (a);
+
+
